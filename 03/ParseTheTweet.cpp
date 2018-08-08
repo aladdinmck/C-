49255ef75827b22b7e71dcc2fe5c9d98dc0eb8ae
aladdin-mck <aladdin320@yahoo.com>
2018-08-08 16:14:20 -0400

    committed work

--- 03/ParseTheTweet.cpp
+++ 03/ParseTheTweet.cpp
@@ -2,10 +2,19 @@
 
 using namespace std;
 
-
-
 int main() {
   string userInput;
+  string delimiter = ";";
   cin >> userInput;
+  cout << "You entered: " << userInput << endl;
+  
+  size_t pos = 0;
+  string token;
+  while ((pos = userInput.find(delimiter)) != string::npos) {
+    token = userInput.substr(0, pos);
+    cout << token << endl;
+    userInput.erase(0, pos + delimiter.length());
+  }
+  cout << userInput << endl;
   return 0;
 }
